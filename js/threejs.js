c6955286d46d4926e227b334c6db930ed345a976
Duc Anh Vu Trinh <ducanhtrinh@usf.edu>
2024-05-27 20:23:50 -0400

    change html

--- js/threejs.js
+++ js/threejs.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { VRMLLoader } from 'three/examples/jsm/loaders/VRMLLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -8,60 +9,80 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// Create an ambient light and add it to the scene
-const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
-scene.add(ambientLight);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-// Create a point light and add it to the scene
-const pointLight = new THREE.PointLight(0xffffff, 1, 1000);
-pointLight.position.set(0, 0, 0);
-scene.add(pointLight);
+// This will enable the damping (inertia) of the controls, which will provide a smoother interaction experience.
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
 
-// Create a GLTFLoader instance
-const loader = new OBJLoader();
+// Disable zooming
+controls.enableZoom = false;
 
-// Load an OBJ resource
+// Disable panning
+controls.enablePan = false;
+
+// Enable rotation
+controls.enableRotate = true;
+// Reduce rotation speed
+controls.rotateSpeed = 0.6;
+
+// Create a directional light and add it to the scene
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(0, 0, 18); // set the position of the light
+scene.add(directionalLight);
+
+// Create a VRMLLoader instance
+const loader = new VRMLLoader();
+
+// Load a VRML resource
 loader.load(
-    // URL of the OBJ resource
-    'asset/3D model/STM32_BUCK_USB-stm32_buck_usb 1.obj',
-    
-    // onLoad callback
-    function (object) {
-        // Scale the object
-        object.scale.set(2, 2, 2);
-        // Set the object's position to the center of the scene
-        const box = new THREE.Box3().setFromObject( object );
-        const center = box.getCenter( new THREE.Vector3() );
+  // URL of the VRML resource
+  'asset/3D model/stm32_buck_usb.wrl',
+  
+  // onLoad callback
+  function (object) {
+    // Scale the object
+    object.scale.set(1, 1, 1);
 
-        object.position.x += ( object.position.x - center.x );
-        object.position.y += ( object.position.y - center.y );
-        object.position.z += ( object.position.z - center.z );
-        // Add a default material to the object
-         object.traverse(function (child) {
-        if (child instanceof THREE.Mesh) {
-          child.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        }
-        });
-        // Add the loaded object to the scene
-        scene.add(object);
-    },
-  
-    // onProgress callback
-    function (xhr) {
-      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    },
-  
-    // onError callback
-    function (error) {
-      console.error('An error occurred while loading the OBJ model:', error);
-    }
-  );
-  
-  camera.position.z = 5;
-  
-  function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+    // Set the object's position to the center of the scene
+    const box = new THREE.Box3().setFromObject( object );
+    const center = box.getCenter( new THREE.Vector3() );
+
+    object.position.x += ( object.position.x - center.x );
+    object.position.y += ( object.position.y - center.y );
+    object.position.z += ( object.position.z - center.z );
+
+    // Add a default material to the object
+    // object.traverse(function (child) {
+    //   if (child instanceof THREE.Mesh) {
+    //     child.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    //   }
+    // });
+
+    // Add the loaded object to the scene
+    scene.add(object);
+  },
+
+  // onProgress callback
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+
+  // onError callback
+  function (error) {
+    console.error('An error occurred while loading the VRML model:', error);
   }
-  
-  animate();
+);
+
+camera.position.z = 55;
+camera.lookAt(scene.position);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // Update the controls in your animation loop.
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
