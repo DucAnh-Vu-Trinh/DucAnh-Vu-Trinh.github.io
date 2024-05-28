4dc153a7ba6a4627a5a69b680b303e6a798f27ed
Duc Anh Vu Trinh <ducanhtrinh@usf.edu>
2024-05-28 01:12:02 -0400

    change html

---
+++ docs/js/threejs.js
@@ -0,0 +1,88 @@
+import * as THREE from 'three';
+import { VRMLLoader } from 'three/examples/jsm/loaders/VRMLLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// This will enable the damping (inertia) of the controls, which will provide a smoother interaction experience.
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+
+// Disable zooming
+controls.enableZoom = false;
+
+// Disable panning
+controls.enablePan = false;
+
+// Enable rotation
+controls.enableRotate = true;
+// Reduce rotation speed
+controls.rotateSpeed = 0.6;
+
+// Create a directional light and add it to the scene
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(0, 0, 18); // set the position of the light
+scene.add(directionalLight);
+
+// Create a VRMLLoader instance
+const loader = new VRMLLoader();
+
+// Load a VRML resource
+loader.load(
+  // URL of the VRML resource
+  'asset/3D model/stm32_buck_usb.wrl',
+  
+  // onLoad callback
+  function (object) {
+    // Scale the object
+    object.scale.set(1, 1, 1);
+
+    // Set the object's position to the center of the scene
+    const box = new THREE.Box3().setFromObject( object );
+    const center = box.getCenter( new THREE.Vector3() );
+
+    object.position.x += ( object.position.x - center.x );
+    object.position.y += ( object.position.y - center.y );
+    object.position.z += ( object.position.z - center.z );
+
+    // Add a default material to the object
+    // object.traverse(function (child) {
+    //   if (child instanceof THREE.Mesh) {
+    //     child.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    //   }
+    // });
+
+    // Add the loaded object to the scene
+    scene.add(object);
+  },
+
+  // onProgress callback
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+
+  // onError callback
+  function (error) {
+    console.error('An error occurred while loading the VRML model:', error);
+  }
+);
+
+camera.position.z = 55;
+camera.lookAt(scene.position);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // Update the controls in your animation loop.
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
