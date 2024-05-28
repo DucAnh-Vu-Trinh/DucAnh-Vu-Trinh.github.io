a10a927cf7d53723a1a518be513c5d5d26a89e82
Duc Anh Vu Trinh <ducanhtrinh@usf.edu>
2024-05-28 11:51:19 -0400

    rebuild for pcb section

---
+++ docs/js/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+    'color-white': '#ffffff',
+    'color-yellow': '#E3AE57',
+    'bg-yellow': '#E3AE57',
+    'bg-white': '#ffffff',
+    'bg-dark': '#161616',
+  };
+
+export default colors;
